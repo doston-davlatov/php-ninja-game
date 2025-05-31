7a25fb8dcbf2e2419cea24744ef92fcd23f34618
iqbolshoh <iilhomjonbov777@gmail.com>
2025-05-31 18:35:43 +0500

    edif file system

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./src/css/style.css">
   <title>Black Ninja</title>
 </head>
 
@@ -16,7 +16,7 @@
     <div id="perfect">Ajoyib</div>
     <button id="restart">RESTART</button>
   </div>
-  <script src="./main.js"></script>
+  <script src="./src/js/main.js"></script>
 </body>
 
 </html>
