f804b09d1713e1b4c7af8e6ce9155e2e1b9b7e94
iqbolshoh <iilhomjonbov777@gmail.com>
2025-06-01 15:27:02 +0500

    edit file

--- game/index.php
+++ game/index.php
@@ -24,7 +24,7 @@ $game_id = $game[0]['id'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/style.css">
+    <link rel="stylesheet" href="../src/css/game.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <title>Black Ninja</title>
@@ -62,7 +62,7 @@ $game_id = $game[0]['id'];
         <button id="restart">RESTART</button>
     </div>
 
-    <script src="../src/js/main.js"></script>
+    <script src="../src/js/game.js"></script>
 </body>
 
 </html>
